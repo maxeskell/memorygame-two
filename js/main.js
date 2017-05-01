12ffb85fd6f38a723de627e76f00fb0cbf0126bc
Max Eskell <maxeskell@Maxs-MBP.home>
2017-05-01 12:00:05 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+   },
+   {
+       rank: "queen",
+       suit: "diamonds",
+       cardImage: "images/queen-of-hearts.png"
+   },
+   {
+       rank: "king",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+   },
+   {
+       rank: "king",
+       suit: "diamonds",
+       cardImage: "images/queen-of-hearts.png"
+   }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
-} else {
+  } else {
     alert("Sorry, try again.");
-}
+  }
 };
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
