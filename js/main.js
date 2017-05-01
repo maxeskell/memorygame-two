06dee7f92ca3882b1de9e26c0e04f0ec6bbd40b6
Max Eskell <maxeskell@Maxs-MBP.home>
2017-05-01 11:05:33 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-console.log('Up and running!');
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo= cards[2];
-cardsInPlay.push(cardOne);
-
-if (cardsInPlay.length === 2) {
- if (cardOne === cardTwo) {
-   alert("You found a match1");
- }
- else {
-   alert("Sorry, try again!");
- }
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
 }
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+flipCard(0);
+flipCard(2);
