fba7cf4b2fd6ceb49aafeac2cf8e89439d62f85e
Max Eskell <maxeskell@Maxs-MBP.home>
2017-05-02 11:36:20 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ var cards = [
    {
        rank: "queen",
        suit: "diamonds",
-       cardImage: "images/queen-of-hearts.png"
+       cardImage: "images/queen-of-diamonds.png"
    },
    {
        rank: "king",
        suit: "hearts",
-       cardImage: "images/queen-of-hearts.png"
+       cardImage: "images/king-of-hearts.png"
    },
    {
        rank: "king",
        suit: "diamonds",
-       cardImage: "images/queen-of-hearts.png"
+       cardImage: "images/king-of-diamonds.png"
    }
 ];
 
@@ -32,14 +32,13 @@ var checkForMatch = function () {
 };
 
 var flipCard = function () {
-  var cardId = this.getAttribute("data-id");
+  cardId = this.getAttribute("data-id");
+  console.log(cardId);
   cardsInPlay.push(cards[cardId].rank);
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
   this.setAttribute("src", cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
+    cardsInPlay = [];
   }
 };
 
